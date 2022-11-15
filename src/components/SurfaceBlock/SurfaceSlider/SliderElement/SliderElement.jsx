90645f1002f97cdee9d623a34ada10b10de76c61
Kidd Kaine <humans.vlone@gmail.com>
2022-11-15 18:14:53 +0500

    0.03 Подготовка слайдера к React-Redux

---
+++ src/components/SurfaceBlock/SurfaceSlider/SliderElement/SliderElement.jsx
@@ -0,0 +1,20 @@
+import classes from './SliderElement.module.scss';
+import { NavLink } from "react-router-dom";
+import Button from "@mui/material/Button";
+import FirstIcon from "../../../../assets/SurfaceSliderIcons/FirstIcon.svg";
+
+const SliderElement = props => (
+    <article className={classes.item} key={ props.id }>
+        <div className={classes.content}>
+            <h2 className={classes.title}>{ props.title }</h2>
+            <p className={classes.text}>{ props.text }</p>
+            <nav>
+                <NavLink to={ props.link } className={classes.link}>
+                    <Button className={classes.button} variant="contained">Перейти</Button>
+                </NavLink>
+            </nav>
+        </div>
+        <img className={classes.image} src={ props.image } alt={'Фотография'} />
+    </article>
+);
+export default SliderElement;
--- src/components/SurfaceBlock/SurfaceSlider/SurfaceSlider.jsx
+++ src/components/SurfaceBlock/SurfaceSlider/SurfaceSlider.jsx
@@ -1,7 +1,6 @@
 import classes from './SurfaceSlider.module.scss';
-import { NavLink } from "react-router-dom";
-import Button from "@mui/material/Button";
 import Slider from "react-slick";
+import SliderElement from "./SliderElement/SliderElement";
 
 const settings = {
     dots: true,
@@ -15,31 +14,6 @@ const settings = {
 };
 
 const SurfaceSlider = () => (
-    <Slider className={classes.slider} {...settings}>
-        <article className={classes.item} key={'slide1'}>
-            <div className={classes.content}>
-                <h2 className={classes.title}>Lorem ipsum.</h2>
-                <p className={classes.text}>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aut culpa cupiditate deserunt doloremque expedita nesciunt, quod reiciendis. Accusantium, aperiam delectus eligendi expedita in odit quod.</p>
-                <nav>
-                    <NavLink to={'/'} className={classes.link}>
-                        <Button className={classes.button} variant="contained">Главная</Button>
-                    </NavLink>
-                </nav>
-            </div>
-            <img className={classes.image} src={'https://placehold.co/1000'} alt={'Фотография'} />
-        </article>
-        <article className={classes.item} key={'slide2'}>
-            <div className={classes.content}>
-                <h2 className={classes.title}>Lorem ipsum.</h2>
-                <p className={classes.text}>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aut culpa cupiditate deserunt doloremque expedita nesciunt, quod reiciendis. Accusantium, aperiam delectus eligendi expedita in odit quod.</p>
-                <nav>
-                    <NavLink to={'/'} className={classes.link}>
-                        <Button className={classes.button} variant="contained">Главная</Button>
-                    </NavLink>
-                </nav>
-            </div>
-            <img className={classes.image} src={'https://placehold.co/1000'} alt={'Фотография'} />
-        </article>
-    </Slider>
+    <Slider className={classes.slider} {...settings}></Slider>
 );
 export default SurfaceSlider;
