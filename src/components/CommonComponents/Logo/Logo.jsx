79862222c02fd1225121ad67598f0056656ca5b1
Kidd Kaine <humans.vlone@gmail.com>
2022-12-23 20:09:41 +0500

    0.236 Замена иконок веб-сайта

--- src/components/CommonComponents/Logo/Logo.jsx
+++ src/components/CommonComponents/Logo/Logo.jsx
@@ -1,11 +1,11 @@
 import classes from './Logo.module.scss';
 import { NavLink } from "react-router-dom";
-import LogoIcon from "./logo.svg";
+import LogoIcon from "../../../assets/logo.svg";
 
 const Logo = () => (
     <NavLink to={'/'} className={classes.link}>
         <img className={classes.icon}
-             src={LogoIcon}
+             src={ LogoIcon }
              alt={'Логотип'}
         />
         <h2 className={classes.title}>Биржа труда</h2>
