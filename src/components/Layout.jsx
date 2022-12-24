43a3ab4d56f9aca6e012e9f550c61870c4a66e7a
Kidd Kaine <humans.vlone@gmail.com>
2022-12-24 16:06:24 +0500

    0.26 Добавление BackToTopButton.jsx + плавный скролл

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -3,14 +3,16 @@ import Header from "./Header/Header";
 import Footer from "./Footer/Footer";
 import { Fade } from "react-awesome-reveal";
 import { Outlet } from "react-router-dom";
+import { BackToTopButton } from "./CommonComponents/BackToTopButton";
 
 const Layout = () => (
-    <div className={classes.wrapper}>
+    <div className={classes.wrapper} id={'anchor'}>
         <Header />
         <Fade className={classes.content} triggerOnce>
             <Outlet />
         </Fade>
         <Footer />
+        <BackToTopButton />
     </div>
 );
 export default Layout;
