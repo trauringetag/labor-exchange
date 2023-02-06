cf45c7c9fe33b07c2b6e8f3902ff310264c3021f
Kidd Kaine <humans.vlone@gmail.com>
2023-02-06 12:16:32 +0500

    0.32 Финальная вёрстка страницы Interview

--- src/components/CommonComponents/LinkButton.jsx
+++ src/components/CommonComponents/LinkButton.jsx
@@ -1,10 +1,10 @@
 import classes from './LinkButton.module.scss';
-import { NavLink } from "react-router-dom";
+import { Link } from "react-router-dom";
 import Button from "@mui/material/Button";
 
 const LinkButton = props => (
-    <NavLink className={classes.link} to={ props.to }>
+    <Link className={classes.link} to={ props.to }>
         <Button className={classes.button} variant="contained">{ props.inscription }</Button>
-    </NavLink>
+    </Link>
 );
 export default LinkButton;
--- src/pages/Interview/Interview.jsx
+++ src/pages/Interview/Interview.jsx
@@ -1,4 +1,6 @@
 import classes from './Interview.module.scss';
+import { Link } from 'react-router-dom';
+import Button from "@mui/material/Button";
 
 const Interview = () => (
     <section className={classes.wrapper}>
@@ -8,6 +10,23 @@ const Interview = () => (
             <p className={`${classes.text} ${classes.bold}`}>Просим граждан и работодателей принять участие в опросе.</p>
             <p className={`${classes.text} ${classes.bold}`}>Центр занятости населения города Карталы проводит опрос граждан и работодателей по удовлетворенности полнотой, доступностью и качеством получаемых государственных услуг.</p>
             <p className={classes.text}>Чтобы принять участие в опросе необходимо скачать формы опросников, заполнить их в электронном виде и направить в Центр занятости населения на адрес электронной почты <a className={classes.link} href={'mailto:krt@szn74.ru'}>krt@szn74.ru</a>.</p>
+        </div>
+        <ul>
+            <li className={classes.item}>
+                <Link to={'/documents/для граждан.doc'} target={'_blank'} download>
+                    <Button className={classes.button} variant="contained">Для граждан</Button>
+                </Link>
+            </li>
+            <li className={classes.item}>
+                <Link to={'/documents/для работадателей.doc'} target={'_blank'} download>
+                    <Button className={classes.button} variant="contained">Для работадателей</Button>
+                </Link>
+            </li>
+        </ul>
+        <div>
+            <p className={`${classes.text} ${classes.bold}`}>Полученные данные являются строго конфиденциальными и будут использованы только для анализа работы Службы занятости населения региона.</p>
+            <p className={`${classes.text} ${classes.italic}`}>Время прохождения: ~ 5-10 минут.</p>
+            <p className={classes.text}>Большое спасибо за сотрудничество!</p>
         </div>
     </section>
 );
