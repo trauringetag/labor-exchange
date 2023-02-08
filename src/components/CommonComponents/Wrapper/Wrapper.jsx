bf710f7f637ee545d8d652a733f754e9606620a9
Kidd Kaine <humans.vlone@gmail.com>
2023-02-08 17:14:18 +0500

    0.381 Разработка goBack function

--- src/components/CommonComponents/Wrapper/Wrapper.jsx
+++ src/components/CommonComponents/Wrapper/Wrapper.jsx
@@ -1,12 +1,29 @@
 import classes from './Wrapper.module.scss';
+import ArrowBackIcon from '@mui/icons-material/ArrowBack';
+import { useLocation, useNavigate } from 'react-router-dom';
+import Button from "@mui/material/Button";
 
-const Wrapper = ({ children, title }) => (
-    <section className={classes.wrapper}>
-        <div>
-            <h2 className={classes.title}>{ title }</h2>
-        </div>
-        <hr className={classes.underlining} />
-        { children }
-    </section>
-);
+const Wrapper = ({ children, title }) => {
+    const location = useLocation();
+    const navigate = useNavigate();
+
+    const goBack = () => navigate(-1);
+
+    return (
+        <section className={classes.container}>
+            <div className={classes.header}>
+                <h2 className={classes.title}>{ title }</h2>
+                {
+                    location.pathname !== '/'
+                        ? <Button className={classes.button} onClick={ goBack }>
+                            <ArrowBackIcon fontSize={'small'}/>
+                            <span>Назад</span>
+                        </Button> : <></>
+                }
+            </div>
+            <hr className={classes.underlining} />
+            { children }
+        </section>
+    );
+};
 export default Wrapper;
