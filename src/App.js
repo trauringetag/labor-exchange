f1ae872503b8716ed1fda09b97731d0911da6f1e
Kidd Kaine <humans.vlone@gmail.com>
2023-02-09 16:26:38 +0500

    0.43 Скролл при смене URL

--- src/App.js
+++ src/App.js
@@ -9,21 +9,31 @@ import AppealProcedure from "./pages/AppealProcedure/AppealProcedure";
 import LegislativeChanges from "./pages/LegislativeChanges/LegislativeChanges";
 import TemporaryEmployment from "./pages/TemporaryEmployment/TemporaryEmployment";
 import ForEmployers from "./pages/ForEmployers/ForEmployers";
+import { useEffect } from 'react';
+import { useLocation } from "react-router-dom";
 
-const App = () => (
-    <Routes>
-        <Route to={'/'} element={<Layout />}>
-            <Route index element={<Official />} />
-            <Route path={'labor-exchange'} element={<Navigate to={'/'} />} />
-            <Route path={'*'} element={<PageNotFound />} />
-            <Route path={'about'} element={<About />} />
-            <Route path={'questionnaires'} element={<Questionnaires />} />
-            <Route path={'legislation'} element={<Legislation />} />
-            <Route path={'legislation/appeal-procedure'} element={<AppealProcedure />} />
-            <Route path={'legislation/legislative-changes'} element={<LegislativeChanges />} />
-            <Route path={'temporary-employment'} element={<TemporaryEmployment />} />
-            <Route path={'for-employers'} element={<ForEmployers />} />
-        </Route>
-    </Routes>
-);
+const App = () => {
+    const location = useLocation();
+
+    useEffect(() => {
+        window.scrollTo(0, 0);
+    }, [location]);
+
+    return (
+        <Routes>
+            <Route to={'/'} element={<Layout />}>
+                <Route index element={<Official />} />
+                <Route path={'labor-exchange'} element={<Navigate to={'/'} />} />
+                <Route path={'*'} element={<PageNotFound />} />
+                <Route path={'about'} element={<About />} />
+                <Route path={'questionnaires'} element={<Questionnaires />} />
+                <Route path={'legislation'} element={<Legislation />} />
+                <Route path={'legislation/appeal-procedure'} element={<AppealProcedure />} />
+                <Route path={'legislation/legislative-changes'} element={<LegislativeChanges />} />
+                <Route path={'temporary-employment'} element={<TemporaryEmployment />} />
+                <Route path={'for-employers'} element={<ForEmployers />} />
+            </Route>
+        </Routes>
+    );
+};
 export default App;
