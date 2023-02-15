d45cc2502d95f75e941f21838977c35a30a24dfb
Kidd Kaine <humans.vlone@gmail.com>
2023-02-15 21:46:43 +0500

    0.44 Настройка UX, скролл к контенту

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { Navigate, Route, Routes, useLocation } from "react-router-dom";
+import { Navigate, Route, Routes } from "react-router-dom";
 import PageNotFound from "./pages/PageNotFound/PageNotFound";
 import Official from "./pages/Official/Official";
 import About from "./pages/About/About";
@@ -9,27 +9,21 @@ import AppealProcedure from "./pages/AppealProcedure/AppealProcedure";
 import LegislativeChanges from "./pages/LegislativeChanges/LegislativeChanges";
 import TemporaryEmployment from "./pages/TemporaryEmployment/TemporaryEmployment";
 import ForEmployers from "./pages/ForEmployers/ForEmployers";
-import { useEffect } from "react";
 
-const App = () => {
-    const location = useLocation();
-    useEffect(() => { window.scrollTo(0, 0); }, [location]);
-
-    return (
-        <Routes>
-            <Route to={'/'} element={<Layout />}>
-                <Route index element={<Official />} />
-                <Route path={'labor-exchange'} element={<Navigate to={'/'} />} />
-                <Route path={'*'} element={<PageNotFound />} />
-                <Route path={'about'} element={<About />} />
-                <Route path={'questionnaires'} element={<Questionnaires />} />
-                <Route path={'legislation'} element={<Legislation />} />
-                <Route path={'legislation/appeal-procedure'} element={<AppealProcedure />} />
-                <Route path={'legislation/legislative-changes'} element={<LegislativeChanges />} />
-                <Route path={'temporary-employment'} element={<TemporaryEmployment />} />
-                <Route path={'for-employers'} element={<ForEmployers />} />
-            </Route>
-        </Routes>
-    );
-};
+const App = () => (
+    <Routes>
+        <Route to={'/'} element={<Layout />}>
+            <Route index element={<Official />} />
+            <Route path={'labor-exchange'} element={<Navigate to={'/'} />} />
+            <Route path={'*'} element={<PageNotFound />} />
+            <Route path={'about'} element={<About />} />
+            <Route path={'questionnaires'} element={<Questionnaires />} />
+            <Route path={'legislation'} element={<Legislation />} />
+            <Route path={'legislation/appeal-procedure'} element={<AppealProcedure />} />
+            <Route path={'legislation/legislative-changes'} element={<LegislativeChanges />} />
+            <Route path={'temporary-employment'} element={<TemporaryEmployment />} />
+            <Route path={'for-employers'} element={<ForEmployers />} />
+        </Route>
+    </Routes>
+);
 export default App;
--- src/components/CommonComponents/Wrapper/Wrapper.jsx
+++ src/components/CommonComponents/Wrapper/Wrapper.jsx
@@ -2,14 +2,21 @@ import classes from './Wrapper.module.scss';
 import ArrowBackIcon from '@mui/icons-material/ArrowBack';
 import { useLocation, useNavigate } from 'react-router-dom';
 import Button from "@mui/material/Button";
+import { useRef, useEffect } from 'react';
 
 const Wrapper = ({ children, title }) => {
+    const ref = useRef(null);
     const location = useLocation();
     const navigate = useNavigate();
     const goBack = () => navigate(-1);
 
+    useEffect(() => {
+        const position = ref.current.getBoundingClientRect();
+        window.scrollBy(0, position.top - 100);
+    }, [location]);
+
     return (
-        <section className={classes.container}>
+        <section className={classes.container} ref={ref}>
             <div className={classes.header}>
                 <h2 className={classes.title}>{ title }</h2>
                 {
