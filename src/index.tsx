a2255b423b52ad7b9051753b129838b911440f30
Kidd Kaine <humans.vlone@gmail.com>
2023-02-27 18:14:34 +0500

    0.47 Внедрение TypeScript в проект

--- src/index.tsx
+++ src/index.tsx
@@ -7,7 +7,10 @@ import { BrowserRouter } from "react-router-dom";
 import { Provider } from "react-redux";
 import { store } from "./store/store";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(
+    document.getElementById('root') as HTMLElement
+);
+
 root.render(
     <BrowserRouter>
         <Provider store={ store }>
