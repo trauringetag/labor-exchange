99e4c91e04c57b2d101d42c799975c92e1d30486
Kidd Kaine <humans.vlone@gmail.com>
2023-04-29 19:00:23 +0500

    0.67 Рефакторинг devDependencies

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { Navigate, Route, Routes } from "react-router-dom";
+import { Route, Routes } from "react-router-dom";
 import PageNotFound from "./pages/PageNotFound/PageNotFound";
 import Official from "./pages/Official/Official";
 import About from "./pages/About/About";
@@ -19,7 +19,6 @@ const App: FC = (): JSX.Element => (
     <Routes>
         <Route path={'/'} element={<Layout />}>
             <Route index element={<Official />} />
-            <Route path={'labor-exchange'} element={<Navigate to={'/'} />} />
             <Route path={'*'} element={<PageNotFound />} />
             <Route path={'about'} element={<About />} />
             <Route path={'questionnaires'} element={<Questionnaires />} />
